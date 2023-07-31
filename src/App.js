569d1e99be3a65afdfc75e4fba149a7838b70675
Cesar Herrera <cesarherr321@gmail.com>
2023-07-30 21:00:22 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {  createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 
 export default App;
