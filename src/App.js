b37584473c4e89a28582e140fd3fae5943153fc2
Cesar Herrera <cesarherr321@gmail.com>
2023-07-30 21:17:36 -0400

    Delete not used files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import {  createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './routes/error-page';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "contacts/:contactId",
+        path: 'contacts/:contactId',
         element: <Contact />,
       },
     ],
   },
 ]);
 
- <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+function App() {
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
 
 export default App;
