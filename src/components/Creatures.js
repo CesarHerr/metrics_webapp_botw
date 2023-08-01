672bf9d552aa269dc577a711366bd23d83425278
Cesar Herrera <cesarherr321@gmail.com>
2023-08-01 12:22:51 -0400

    Add creatures category

--- src/components/Creatures.js
+++ src/components/Creatures.js
@@ -1,6 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchCreature } from '../redux/botw/Api';
+import { setClickedCardId } from '../redux/botw/botwSlice';
+import Card from './Card';
+
 function Creatures() {
+  const dispatch = useDispatch();
+  const { creatures } = useSelector((state) => state.cards);
+
+  useEffect(() => {
+    if (creatures.length < 1) {
+      dispatch(fetchCreature());
+    }
+  }, [dispatch, creatures.length]);
+
+  const handleClick = (id) => {
+    dispatch(setClickedCardId(id));
+  };
+
   return (
-    <h1>Creatures 1</h1>
+    <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {creatures
+        .map((card) => (
+          <Card
+            key={card.id}
+            image={card.image}
+            category={card.category}
+            name={card.name}
+            handleClick={() => handleClick(card.id)}
+          />
+        ))
+        .slice(0, 100)}
+    </ul>
   );
 }
 
