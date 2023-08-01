076b7454ebc7552e2b62e7eb9dbafb1da77a0d26
Cesar Herrera <cesarherr321@gmail.com>
2023-08-01 12:24:10 -0400

    Add treasure category

--- src/components/Tresures.js
+++ src/components/Tresures.js
@@ -1,6 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTreasures } from '../redux/botw/Api';
+import { setClickedCardId } from '../redux/botw/botwSlice';
+import Card from './Card';
+
 function Treasures() {
+  const dispatch = useDispatch();
+  const { treasures } = useSelector((state) => state.cards);
+
+  useEffect(() => {
+    if (treasures.length === 0) {
+      dispatch(fetchTreasures());
+    }
+  }, [dispatch, treasures.length]);
+
+  const handleClick = (id) => {
+    dispatch(setClickedCardId(id));
+  };
+
   return (
-    <h1>Treasures 1</h1>
+    <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {treasures
+        .map((card) => (
+          <Card
+            key={card.id}
+            image={card.image}
+            category={card.category}
+            name={card.name}
+            handleClick={() => handleClick(card.id)}
+          />
+        ))
+        .slice(0, 100)}
+    </ul>
   );
 }
 
