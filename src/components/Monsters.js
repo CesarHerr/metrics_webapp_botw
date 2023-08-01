f688df326955d4a4524cfd21f0da4ffbfb03b322
Cesar Herrera <cesarherr321@gmail.com>
2023-08-01 12:23:47 -0400

    Add monsters category

--- src/components/Monsters.js
+++ src/components/Monsters.js
@@ -1,6 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchMonsters } from '../redux/botw/Api';
+import { setClickedCardId } from '../redux/botw/botwSlice';
+import Card from './Card';
+
 function Monsters() {
+  const dispatch = useDispatch();
+  const { monsters } = useSelector((state) => state.cards);
+
+  useEffect(() => {
+    if (monsters.length === 0) {
+      dispatch(fetchMonsters());
+    }
+  }, [dispatch, monsters.length]);
+
+  const handleClick = (id) => {
+    dispatch(setClickedCardId(id));
+  };
+
   return (
-    <h1>Monsters 1</h1>
+    <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {monsters
+        .map((card) => (
+          <Card
+            key={card.id}
+            image={card.image}
+            category={card.category}
+            name={card.name}
+            handleClick={() => handleClick(card.id)}
+          />
+        ))
+        .slice(0, 100)}
+    </ul>
   );
 }
 
