fb7dc33cea92cd72b9e660f7c611ec379b96e040
Cesar Herrera <cesarherr321@gmail.com>
2023-08-01 12:23:12 -0400

    Add equipment category

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -1,6 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchEquipment } from '../redux/botw/Api';
+import { setClickedCardId } from '../redux/botw/botwSlice';
+import Card from './Card';
+
 function Equipment() {
+  const dispatch = useDispatch();
+  const { equipment } = useSelector((state) => state.cards);
+
+  useEffect(() => {
+    if (equipment.length === 0) {
+      dispatch(fetchEquipment());
+    }
+  }, [dispatch, equipment.length]);
+
+  const handleClick = (id) => {
+    dispatch(setClickedCardId(id));
+  };
+
   return (
-    <h1>Equipment 1</h1>
+    <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {equipment
+        .map((card) => (
+          <Card
+            key={card.id}
+            image={card.image}
+            category={card.category}
+            name={card.name}
+            handleClick={() => handleClick(card.id)}
+          />
+        ))
+        .slice(0, 100)}
+    </ul>
   );
 }
 
