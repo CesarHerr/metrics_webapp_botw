3da64048f32d04e0bd12254c11c49e1e0b7f407c
Cesar Herrera <cesarherr321@gmail.com>
2023-08-01 12:21:53 -0400

    Add a handle click function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,27 @@
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
-function Card({ name, image, category }) {
+function Card({
+  name, image, category, handleClick,
+}) {
   return (
     <li>
-      <ul>
-        <li>
-          <h3>{name}</h3>
-        </li>
-        <li>
-          <img src={image} alt="descriptive info about" />
-        </li>
-        <li>
-          <h4>{category}</h4>
-        </li>
-      </ul>
+      <NavLink onClick={handleClick} to="../card_detail">
+        <span style={{ fontSize: '2rem' }}>
+          ▶️ ➡️
+        </span>
+        <ul>
+          <li>
+            <h3>{name}</h3>
+          </li>
+          <li>
+            <img src={image} alt="descriptive info about" />
+          </li>
+          <li>
+            <h4>{category}</h4>
+          </li>
+        </ul>
+      </NavLink>
     </li>
   );
 }
@@ -22,6 +30,7 @@ Card.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Card;
