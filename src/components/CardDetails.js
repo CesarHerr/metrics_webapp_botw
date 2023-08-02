e004e0143b4a6c68d930e5a1975eed2b8ec4fd69
Cesar Herrera <cesarherr321@gmail.com>
2023-08-01 20:45:25 -0400

    Add styles to the cardDetails component

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchDetail } from '../redux/botw/Api';
+import '../styles/CardDetails.css';
+import heart from '../images/heart.png';
 
 function CardDetails() {
   const { clickedCardId, detail } = useSelector((state) => state.cards);
@@ -13,59 +15,58 @@ function CardDetails() {
   }, [dispatch, clickedCardId]);
 
   return (
-    <section
-      style={{
-        backgroundColor: 'rgb(100,100,100)',
-        display: 'inline',
-        width: '100%',
-        height: '1000px',
-      }}
-    >
-      <h2>detail</h2>
-      <ul>
+    <section className="cardDetails">
+      <h2>{detail.name ? detail.name[0].toUpperCase() + detail.name.substring(1) : 'Name'}</h2>
+      <h6>
+        Category :
+        {' '}
+        {detail.category}
+      </h6>
+      <p>{detail.description}</p>
+      <img className="creatureImg" src={detail.image} alt="creature detail" />
+      <span className="locations">
+        <h3>
+          Common Locations:
+        </h3>
+        <h3>
+          {detail.common_locations
+            ? ` ${detail.common_locations.join(', ')[0].toUpperCase()}${detail.common_locations.join(', ').substring(1)}`
+            : 'Unknown'}
+        </h3>
+      </span>
+      <ul className="infoCard">
         <li>
-          <h2>
-            {detail.category}
+
+          <h4> Power: </h4>
+          <h5>{detail.id}</h5>
+        </li>
+        <li>
+          <h4>Hearts recovered:</h4>
+          <h5>{detail.hearts_recovered || <i>No recover</i>}</h5>
+          <img className="heart" src={heart} alt="heart" />
+        </li>
+        <li>
+          <h4>Drops:</h4>
+          <h5>{detail.drops ? detail.drops.join(', ')[0].toUpperCase() + detail.drops.join(', ') : <i>No drops</i> }</h5>
+
+        </li>
+        <li>
+          <h4>
+            Cooking effect:
+            {detail.cooking_effect || <h5><i>No effect</i></h5>}
+          </h4>
+        </li>
+        <li>
+          <h4>
+            Attack:
             {' '}
-            <span style={{ fontSize: '3rem' }}>{detail.name}</span>
-          </h2>
-        </li>
-        <li><p>{detail.description}</p></li>
-        <li>
-          <img src={detail.image} alt="creature" />
-        </li>
-        <li>
-          <h3>
-            Common Locations:
-            {detail.common_locations ? detail.common_locations.join(', ') : 'Unknown'}
-          </h3>
-        </li>
-        <li>
-          Power:
-          <h3>{detail.id}</h3>
-        </li>
-        <li>
-          Hearts recovered:
-          <h3>{detail.hearts_recovered || 'No recover'}</h3>
-        </li>
-        <li>
-          <h3>
-            Drops:
-            {detail.drops ? detail.drops.join(', ') : 'No drops'}
-          </h3>
-        </li>
-        <li>
-          <h3>
-            cooking effect:
-            {detail.cooking_effect || ' No effect'}
-          </h3>
-        </li>
-        <li>
-          <h3>
-            Properties:
-            {detail.properties ? Object.keys(detail.properties).join(', ') : ' No effect'}
-            {detail.properties ? Object.values(detail.properties).join(', ') : 'No'}
-          </h3>
+            {detail.properties ? Object.values(detail.properties)[0] : <h5><i>No Attack</i></h5>}
+          </h4>
+          <h4>
+            Defense:
+            {' '}
+            {detail.properties ? Object.values(detail.properties)[1] : <h5><i>No Defense</i></h5>}
+          </h4>
         </li>
       </ul>
     </section>
