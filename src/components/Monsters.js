f23451f412b740cfb00cdf36475cc155267719ff
Cesar Herrera <cesarherr321@gmail.com>
2023-08-02 19:02:04 -0400

    Add header to the materials category

--- src/components/Monsters.js
+++ src/components/Monsters.js
@@ -19,20 +19,29 @@ function Monsters() {
   };
 
   return (
-    <ul className="itemsList">
-      <h2>Monsters</h2>
-      {monsters
-        .map((card) => (
-          <Card
-            key={card.id}
-            image={card.image}
-            category={card.category}
-            name={card.name[0].toUpperCase() + card.name.substring(1)}
-            handleClick={() => handleClick(card.id)}
-          />
-        ))
-        .slice(0, 100)}
-    </ul>
+    <section className="selectCardSection">
+      <div className="selectCardSection__monsters">
+        <span className="material-symbols-outlined">
+          arrow_circle_right
+        </span>
+        <div>
+          <h2>Monsters</h2>
+          <h3>80</h3>
+        </div>
+      </div>
+      <ul className="itemsList">
+        {monsters
+          .map((card) => (
+            <Card
+              key={card.id}
+              image={card.image}
+              category={card.category}
+              name={card.name[0].toUpperCase() + card.name.substring(1)}
+              handleClick={() => handleClick(card.id)}
+            />
+          ))}
+      </ul>
+    </section>
   );
 }
 
