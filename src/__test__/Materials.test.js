e9c91830a2e21a55dd148fb9d5a19ff3ae4c58f2
Cesar Herrera <cesarherr321@gmail.com>
2023-08-02 21:12:55 -0400

    Add test to Materials component

---
+++ src/__test__/Materials.test.js
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import Materials from '../components/Materials';
+
+const mockStore = configureStore([]);
+
+describe('Materials render test', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      cards: {
+        materials: [
+          {
+            id: '1',
+            name: 'materials 1',
+            category: 'materials',
+            image: '../images/materials.png',
+          },
+          {
+            id: '2',
+            name: 'materials 2',
+            category: 'materials',
+            image: '../images/materials.png',
+          },
+        ],
+      },
+    });
+  });
+
+  test('renders materials list correctly', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter> 
+          <Materials />
+        </MemoryRouter> 
+      </Provider>,
+    );
+
+    expect(screen.getByText('Materials 1')).toBeInTheDocument();
+    expect(screen.getByText('Materials 2')).toBeInTheDocument();
+    expect(screen.getByText('Materials')).toBeInTheDocument();
+
+    const image1 = screen.getByAltText("Materials 1");
+    const image2 = screen.getByAltText("Materials 2");
+
+    expect(image1).toBeInTheDocument();
+    expect(image2).toBeInTheDocument();
+  });
+});
