bbe2a5306ef312737598414e54fd762c07c97e2e
Cesar Herrera <cesarherr321@gmail.com>
2023-08-09 21:36:46 -0400

    Add desktop styles for monster and treasures

--- src/components/Monsters.js
+++ src/components/Monsters.js
@@ -1,8 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { fetchMonsters } from '../redux/botw/Api';
 import { setClickedCardId } from '../redux/botw/botwSlice';
 import Card from './Card';
+import CardDetails from './CardDetails';
+import sword from '../images/masterSword4.png';
 
 function Monsters() {
   const dispatch = useDispatch();
@@ -18,24 +21,37 @@ function Monsters() {
     dispatch(setClickedCardId(id));
   };
 
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    if (navigate) {
+      navigate(-1);
+    }
+  };
+
   return (
-    <section className="selectCardSection">
-      <div className="selectCardSection__monsters">
-        <h2>Monsters</h2>
-        <h3>80</h3>
-      </div>
-      <ul className="itemsList">
-        {monsters.map((card) => (
-          <Card
-            key={card.id}
-            image={card.image}
-            category={card.category}
-            name={card.name[0].toUpperCase() + card.name.substring(1)}
-            handleClick={() => handleClick(card.id)}
-          />
-        ))}
-      </ul>
-    </section>
+    <div className="infoMenu">
+      <section className="selectCardSection">
+        <div className="selectCardSection__monsters selectCardSection__all">
+          <h2>Monsters</h2>
+          <h3>80</h3>
+          <button type="button" onClick={handleGoBack} className="selectCardSection__navButton">
+            <img className="selectCardSection__masterSword" src={sword} alt="master sword" />
+          </button>
+        </div>
+        <ul className="itemsList">
+          {monsters.map((card) => (
+            <Card
+              key={card.id}
+              image={card.image}
+              category={card.category}
+              name={card.name[0].toUpperCase() + card.name.substring(1)}
+              handleClick={() => handleClick(card.id)}
+            />
+          ))}
+        </ul>
+      </section>
+      <CardDetails />
+    </div>
   );
 }
 
--- src/components/Treasures.js
+++ src/components/Treasures.js
@@ -1,9 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { fetchTreasures } from '../redux/botw/Api';
 import { setClickedCardId } from '../redux/botw/botwSlice';
 import Card from './Card';
 import '../styles/Creatures.css';
+import CardDetails from './CardDetails';
+import sword from '../images/masterSword4.png';
 
 function Treasures() {
   const dispatch = useDispatch();
@@ -19,24 +22,37 @@ function Treasures() {
     dispatch(setClickedCardId(id));
   };
 
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    if (navigate) {
+      navigate(-1);
+    }
+  };
+
   return (
-    <section className="selectCardSection">
-      <div className="selectCardSection__treasures">
-        <h2>Treasures</h2>
-        <h3>4</h3>
-      </div>
-      <ul className="itemsList">
-        {treasures.map((card) => (
-          <Card
-            key={card.id}
-            image={card.image}
-            category={card.category}
-            name={card.name[0].toUpperCase() + card.name.substring(1)}
-            handleClick={() => handleClick(card.id)}
-          />
-        ))}
-      </ul>
-    </section>
+    <div className="infoMenu">
+      <section className="selectCardSection">
+        <div className="selectCardSection__treasures selectCardSection__all">
+          <h2>Treasures</h2>
+          <h3>4</h3>
+          <button type="button" onClick={handleGoBack} className="selectCardSection__navButton">
+            <img className="selectCardSection__masterSword" src={sword} alt="master sword" />
+          </button>
+        </div>
+        <ul className="itemsList">
+          {treasures.map((card) => (
+            <Card
+              key={card.id}
+              image={card.image}
+              category={card.category}
+              name={card.name[0].toUpperCase() + card.name.substring(1)}
+              handleClick={() => handleClick(card.id)}
+            />
+          ))}
+        </ul>
+      </section>
+      <CardDetails />
+    </div>
   );
 }
 
