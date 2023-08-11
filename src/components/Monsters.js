de7c1be33d8287b1b20a87377ae5180b12b3d6b0
Cesar Herrera <cesarherr321@gmail.com>
2023-08-11 12:08:14 -0400

    Add search for Monsters

--- src/components/Monsters.js
+++ src/components/Monsters.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchMonsters, fetchDetail } from '../redux/botw/Api';
 import { setClickedCardId, hideImage, showCardList } from '../redux/botw/botwSlice';
 import Card from './Card';
@@ -13,6 +13,7 @@ function Monsters() {
   const {
     monsters, isCardListVisible, detail, clickedCardId, isImageVisible,
   } = useSelector((state) => state.cards);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     if (monsters.length === 0) {
@@ -41,18 +42,29 @@ function Monsters() {
     dispatch(showCardList());
   };
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const filterList = monsters.filter(
+    (name) => name.name.includes(search),
+  );
+
   return (
     <div className="infoMenu">
       <section className="selectCardSection">
         <div className="selectCardSection__monsters selectCardSection__all">
-          <button type="button" onClick={handleHideImg}><h2>Monsters</h2></button>
+          <div className="selectCardSection__all--search">
+            <button type="button" onClick={handleHideImg}><h2>Creatures</h2></button>
+            <input type="text" placeholder="Type a name" value={search} onChange={handleSearch} />
+          </div>
           <h3>80</h3>
           <button type="button" onClick={handleGoBack} className="selectCardSection__navButton">
             <img className="selectCardSection__masterSword" src={sword} alt="master sword" />
           </button>
         </div>
         <ul className="itemsList" style={{ display: isCardListVisible ? 'flex' : 'none' }}>
-          {monsters.map((card) => (
+          {filterList.length > 0 ? (filterList.map((card) => (
             <Card
               key={card.id}
               image={card.image}
@@ -60,7 +72,13 @@ function Monsters() {
               name={card.name[0].toUpperCase() + card.name.substring(1)}
               handleClick={() => handleClick(card.id)}
             />
-          ))}
+          ))
+          ) : (
+            <>
+              <p>Not Results 😭!! </p>
+              <p>Try another name 😄</p>
+            </>
+          )}
         </ul>
         <div className="selectCardSection__info" style={{ display: isImageVisible ? 'flex' : 'none' }}>
           <img
