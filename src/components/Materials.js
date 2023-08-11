018c0a7441aa319415bbde0214e7c88b6e69d87f
Cesar Herrera <cesarherr321@gmail.com>
2023-08-11 12:08:04 -0400

    Add search for Materials

--- src/components/Materials.js
+++ src/components/Materials.js
@@ -1,8 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchMaterials, fetchDetail } from '../redux/botw/Api';
-import { setClickedCardId, hideImage, showCardList } from '../redux/botw/botwSlice';
+import {
+  setClickedCardId,
+  hideImage,
+  showCardList,
+} from '../redux/botw/botwSlice';
 import Card from './Card';
 import sword from '../images/masterSword4.png';
 import CardDetails from './CardDetails';
@@ -11,8 +15,13 @@ function Materials() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const {
-    materials, isCardListVisible, detail, clickedCardId, isImageVisible,
+    materials,
+    isCardListVisible,
+    detail,
+    clickedCardId,
+    isImageVisible,
   } = useSelector((state) => state.cards);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     if (materials.length === 0) {
@@ -41,28 +50,65 @@ function Materials() {
     dispatch(showCardList());
   };
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const filterList = materials.filter((name) => name.name.includes(search));
+
   return (
     <div className="infoMenu">
       <section className="selectCardSection">
         <div className="selectCardSection__materials selectCardSection__all">
-          <button type="button" onClick={handleHideImg}><h2>Materials</h2></button>
+          <div className="selectCardSection__all--search">
+            <button type="button" onClick={handleHideImg}>
+              <h2>Creatures</h2>
+            </button>
+            <input
+              type="text"
+              placeholder="Type a name"
+              value={search}
+              onChange={handleSearch}
+            />
+          </div>
           <h3>82</h3>
-          <button type="button" onClick={handleGoBack} className="selectCardSection__navButton">
-            <img className="selectCardSection__masterSword" src={sword} alt="master sword" />
+          <button
+            type="button"
+            onClick={handleGoBack}
+            className="selectCardSection__navButton"
+          >
+            <img
+              className="selectCardSection__masterSword"
+              src={sword}
+              alt="master sword"
+            />
           </button>
         </div>
-        <ul className="itemsList" style={{ display: isCardListVisible ? 'flex' : 'none' }}>
-          {materials.map((card) => (
-            <Card
-              key={card.id}
-              image={card.image}
-              category={card.category}
-              name={card.name[0].toUpperCase() + card.name.substring(1)}
-              handleClick={() => handleClick(card.id)}
-            />
-          ))}
+        <ul
+          className="itemsList"
+          style={{ display: isCardListVisible ? 'flex' : 'none' }}
+        >
+          {filterList.length > 0 ? (
+            filterList.map((card) => (
+              <Card
+                key={card.id}
+                image={card.image}
+                category={card.category}
+                name={card.name[0].toUpperCase() + card.name.substring(1)}
+                handleClick={() => handleClick(card.id)}
+              />
+            ))
+          ) : (
+            <>
+              <p>Not Results 😭!! </p>
+              <p>Try another name 😄</p>
+            </>
+          )}
         </ul>
-        <div className="selectCardSection__info" style={{ display: isImageVisible ? 'flex' : 'none' }}>
+        <div
+          className="selectCardSection__info"
+          style={{ display: isImageVisible ? 'flex' : 'none' }}
+        >
           <img
             className="selectCardSection__info--img"
             src={detail.image}
