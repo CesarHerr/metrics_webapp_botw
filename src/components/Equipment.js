41c7ae80cad45c75a3caf3c214745149ac954684
Cesar Herrera <cesarherr321@gmail.com>
2023-11-29 19:41:07 -0300

    Add loader on Equipment component

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -3,17 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { fetchEquipment, fetchDetail } from '../redux/botw/Api';
 import {
-  setClickedCardId, hideImage, showCardList, showSearch,
+  setClickedCardId,
+  hideImage,
+  showCardList,
+  showSearch,
 } from '../redux/botw/botwSlice';
 import Card from './Card';
 import CardDetails from './CardDetails';
 import sword from '../images/masterSword4.png';
+import Spinner from './Spinner';
 
 function Equipment() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const {
-    equipment, isCardListVisible, detail, clickedCardId, isImageVisible, isSearchVisible,
+    equipment,
+    isCardListVisible,
+    detail,
+    clickedCardId,
+    isImageVisible,
+    isSearchVisible,
+    isLoading,
   } = useSelector((state) => state.cards);
   const [search, setSearch] = useState('');
 
@@ -49,41 +59,59 @@ function Equipment() {
     setSearch(event.target.value);
   };
 
-  const filterList = equipment.filter(
-    (name) => name.name.includes(search),
-  );
+  const filterList = equipment.filter((name) => name.name.includes(search));
 
   return (
     <div className="infoMenu">
       <section className="selectCardSection">
         <div className="selectCardSection__equipment selectCardSection__all">
           <div className="selectCardSection__all--search">
-            <button type="button" onClick={handleHideImg}><h2>Equipment</h2></button>
-            <input type="text" placeholder="Type a name" value={search} onChange={handleSearch} style={{ display: isSearchVisible ? 'inline' : 'none' }} />
+            <button type="button" onClick={handleHideImg}>
+              <h2>Equipment</h2>
+            </button>
+            <input
+              type="text"
+              placeholder="Type a name"
+              value={search}
+              onChange={handleSearch}
+              style={{ display: isSearchVisible ? 'inline' : 'none' }}
+            />
           </div>
           <h3>184</h3>
-          <button type="button" onClick={handleGoBack} className="selectCardSection__navButton">
-            <img className="selectCardSection__masterSword" src={sword} alt="master sword" />
+          <button
+            type="button"
+            onClick={handleGoBack}
+            className="selectCardSection__navButton"
+          >
+            <img
+              className="selectCardSection__masterSword"
+              src={sword}
+              alt="master sword"
+            />
           </button>
         </div>
-        <ul className="itemsList" style={{ display: isCardListVisible ? 'flex' : 'none' }}>
-          {filterList.length > 0 ? (filterList.map((card) => (
-            <Card
-              key={card.id}
-              image={card.image}
-              category={card.category}
-              name={card.name[0].toUpperCase() + card.name.substring(1)}
-              handleClick={() => handleClick(card.id)}
-            />
-          ))
+        <ul
+          className="itemsList"
+          style={{ display: isCardListVisible ? 'flex' : 'none' }}
+        >
+          {isLoading && filterList.length <= 0 ? (
+            <Spinner />
           ) : (
-            <>
-              <p>Not Results 😭!! </p>
-              <p>Try another name 😄</p>
-            </>
+            filterList.map((card) => (
+              <Card
+                key={card.id}
+                image={card.image}
+                category={card.category}
+                name={card.name[0].toUpperCase() + card.name.substring(1)}
+                handleClick={() => handleClick(card.id)}
+              />
+            ))
           )}
         </ul>
-        <div className="selectCardSection__info" style={{ display: isImageVisible ? 'flex' : 'none' }}>
+        <div
+          className="selectCardSection__info"
+          style={{ display: isImageVisible ? 'flex' : 'none' }}
+        >
           <img
             className="selectCardSection__info--img"
             src={detail.image}
