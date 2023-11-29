71954dfc9a4d98a719c584ea8d5819eae5f80dde
Cesar Herrera <cesarherr321@gmail.com>
2023-11-29 19:42:05 -0300

    Add spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,21 @@
+import '../styles/Spinner.css';
+
+function Spinner() {
+  return (
+    <>
+      <div className="frame">
+        <div className="dot dot1" />
+        <br />
+        <div className="dot dot2" />
+        <div className="dot dot3" />
+        <br />
+        <div className="dot dot4" />
+        <div className="dot dot5" />
+        <br />
+        <div className="dot dot6" />
+      </div>
+    </>
+  );
+}
+
+export default Spinner;
